fd3678dbe2dacb9041eb65df0979f8067cec595b
Andrew Locke <alocke12992@gmail.com>
2019-02-09 18:22:23 -0800

    Removes uneeded GraphQL Queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { graphql, Link } from 'gatsby'
+import React, { Fragment } from 'react'
+import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import Hero from '../components/Hero'
@@ -14,13 +14,13 @@ class RootIndex extends React.Component {
     const { products } = this.props.data.productQuery;
     return (
       <Layout location={this.props.location}>
-        <div style={{ background: '#fff' }}>
+        <Fragment>
           <Helmet title={siteTitle} />
           <Hero data={author.node} />
           <div>
-            
+
           </div>
-        </div>
+        </Fragment>
       </Layout>
     )
   }
@@ -30,47 +30,6 @@ export default RootIndex
 
 export const pageQuery = graphql`
   query HomeQuery {
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      edges {
-        node {
-          title
-          slug
-          publishDate(formatString: "MMMM Do, YYYY")
-          tags
-          heroImage {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
-          description {
-            childMarkdownRemark {
-              html
-            }
-          }
-        }
-      }
-    }
-    allContentfulPerson(filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }) {
-      edges {
-        node {
-          name
-          shortBio {
-            shortBio
-          }
-          title
-          heroImage: image {
-            fluid(
-              maxWidth: 1180
-              maxHeight: 480
-              resizingBehavior: PAD
-              background: "rgb:000000"
-            ) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
-        }
-      }
-    }
     productQuery: allStripeSku {
       products: edges {
         product: node { 
