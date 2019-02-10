be4d979df9fbd7c040ca0764d1455ab13bbd0ff1
Andrew Locke <alocke12992@gmail.com>
2019-02-09 17:57:52 -0800

    adds navbar scaffold

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,55 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import styles from './navigation.module.css'
+import React from 'react';
+import {
+  NavBar,
+  NavLinks,
+  NavLink,
+} from './styles.js';
+
+const navLinks = [ 
+  {
+    title: 'About',
+    route: 'about/',
+  },
+  {
+    title: 'News',
+    route: 'news/',
+  },
+  {
+    title: 'Products',
+    route: 'products/',
+  },
+  {
+    title: 'Video',
+    route: 'video/',
+  },
+  {
+    title: 'Recruit',
+    route: 'recruit/',
+  },
+  {
+    title: 'FAQ',
+    route: 'faq/',
+  },
+  {
+    title: 'Contact',
+    route: 'contact/',
+  }
+];
 
 export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
-      </li>
-    </ul>
-  </nav>
+  <NavBar role="navigation">
+    <NavLinks className='nav-links'>
+      {
+        navLinks.map((link, index) => (
+          <NavLink
+            key={index}
+            to={link.route}
+          >
+            {link.title}
+          </NavLink>
+        ))
+      }
+    </NavLinks>
+  </NavBar>
 )
+
