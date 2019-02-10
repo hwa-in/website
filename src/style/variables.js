4ecf31d2e2884d573f2bac497425fb6a6eeb84a9
Andrew Locke <alocke12992@gmail.com>
2019-02-09 23:01:24 -0800

    fin footer

--- src/style/variables.js
+++ src/style/variables.js
@@ -1,6 +1,7 @@
 const variables = {
   darkGrey: '#414141',
-  lightGrey: '#6f6f6f',
+  lightGrey: '#c3c3c3',
+  midGrey: '#6e6e6e',
   white: '#FFFFFF',
   lightGreen: '#80ccae',
   darkGreen: '#408d44',
