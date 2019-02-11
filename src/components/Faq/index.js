71d4ef6d31f263a9411bced44f0d7409c90f33be
Karin Hsu <khsu0720@gmail.com>
2019-02-11 14:01:43 +0800

    added FAQs test content and graphql query

---
+++ src/components/Faq/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FAQ = ({title, answer}) => {
+  return <div> {title} + {answer.answer}</div>
+}
+
+export default FAQ;
--- src/pages/faqs/index.js
+++ src/pages/faqs/index.js
@@ -1,15 +1,35 @@
 import React, { Fragment } from 'react';
 import Section from '../../components/styledComponents/Section';
+import FAQ from '../../components/Faq';
 
-const FAQsPage = (data) => {
-  console.log(data)
+const FAQsPage = ({data}) => {
+  const {faqs} = data.allContentfulFaq;
   return (
     <Fragment>
       <Section>
         FAQs
+        {faqs.map(({faq}, index) => {
+          console.log(faq)
+          return <FAQ {...faq} key={index}/>
+        })}
       </Section>
     </Fragment>
   )
 };
 
 export default FAQsPage;
+
+export const faqQuery = graphql`
+query AllFaqs {	
+  allContentfulFaq {
+    faqs: edges {
+      faq: node{
+        title
+        answer {
+          answer
+        }
+      }
+    }
+  }
+}
+`
