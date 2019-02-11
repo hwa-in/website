49e730e14856a200b21d1ec98df92697d7dd0469
Karin Hsu <khsu0720@gmail.com>
2019-02-11 18:57:49 +0800

    basic q&a conditional rendering applied

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import tempFaqStyle from './styles'
+// import { bold } from 'ansi-colors';
 
-const FAQ = ({title, answer}) => {
-  return <div> {title} + {answer.answer}</div>
+
+// export default tempFaqStyle;
+class FAQ extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      clickedOn: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    var value = this.state.clickedOn;
+    if (value) {
+      this.setState({
+        clickedOn: false
+      });
+    } else {
+      this.setState({
+        clickedOn: true
+      });
+    }
+  }
+  render() {
+    const {title, answer} = this.props;
+    const questionSelected = this.state.clickedOn;
+    let showAnswer;
+    if (questionSelected) {
+      showAnswer = <div> {answer.answer}</div>
+    }
+    return (
+    <div>
+    <div style={tempFaqStyle} className='title' onClick={this.handleClick}>{title}</div>
+    {showAnswer}
+    </div>
+    )
+  }
 }
 
 export default FAQ;
