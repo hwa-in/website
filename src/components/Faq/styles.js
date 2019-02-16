a7a24b05083453c3da17254eb51a235e05f93f7b
Karin Hsu <khsu0720@gmail.com>
2019-02-16 12:32:37 +0800

    accordion style added to q&a

--- src/components/Faq/styles.js
+++ src/components/Faq/styles.js
@@ -18,17 +18,28 @@ var FaqStyleTitle = styled.div`
   padding: 15px 20px;
   cursor: pointer;
   transition: background-color 0.2s linear;
-
 `;
 
 var FaqStyleContent = styled.div`: 
-  padding: 15px 20px; 
+  overflow: hidden;
+  color: green;
   border-left: 1px solid ${variables.whitesmoke};
   border-right: 1px solid ${variables.whitesmoke};
-  height: auto;
-  transition: height 0.2s ease-in-out;
-`;
+  transition: all 0.3s ease-out;
+  height: 0;
+  opacity: 0;
+  padding: 0 0.6rem;
+  background: ${variables.whitesmoke};
+  font-weight: 200;
+  letter-spacing: 1px;
 
+  &.open {
+    color: red; 
+    opacity: 1;
+    height: auto;
+    padding: 1rem;
+  }
+`;
 
 export {
   FaqStyleContainer, 
