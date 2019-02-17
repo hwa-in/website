5a77ca6036a4200b2979e7b21971d85bc975c9e2
Andrew Locke <alocke12992@gmail.com>
2019-02-17 01:39:09 -0800

    Fin transition

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -1,34 +1,53 @@
 import React from 'react';
-import {FaqStyleTitle, FaqStyleContent} from './styles'
+import { CSSTransition } from 'react-transition-group';
+import {
+  FaqStyleContainer,
+  FaqStyleTitle,
+  FaqStyleContent,
+} from './styles';
+
+const duration = 500;
 
 class FAQ extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      clickedOn: false
-    }
-    this.handleClick = this.handleClick.bind(this);
+  state = {
+    clickedOn: false,
   }
   
-  handleClick() {
+  handleClick = () => {
     this.setState({
-      clickedOn: !this.state.clickedOn
+      clickedOn: !this.state.clickedOn,
     });
   }
-  render() {
-    const {title, answer} = this.props;
-    const {clickedOn} = this.state
-    return ( 
-      <div> 
-        <FaqStyleTitle onClick={this.handleClick}> 
-          {title} 
-        </FaqStyleTitle> 
-        <FaqStyleContent className={clickedOn ? "open" : ""}> 
-          {answer.answer}
-        </FaqStyleContent> 
-      </div> 
+
+  showAnswer = () => {
+    const { clickedOn } = this.state
+    const { answer } = this.props
+    return (
+      <CSSTransition
+        in={clickedOn}
+        timeout={duration}
+        classNames="answer"
+      >
+        {(state) => (
+          <FaqStyleContent className="answer">
+              <p>{answer.answer}</p>
+          </FaqStyleContent> 
+        )}
+      </CSSTransition>
     )
-    }
   }
 
-  export default FAQ;
+  render() {
+    const { title } = this.props;
+    return ( 
+      <FaqStyleContainer onClick={ () => this.handleClick() }> 
+        <FaqStyleTitle> 
+          { title }
+        </FaqStyleTitle>
+        { this.showAnswer() }
+      </FaqStyleContainer> 
+    )
+  }
+}
+
+export default FAQ;
--- src/components/Faq/styles.js
+++ src/components/Faq/styles.js
@@ -1,43 +1,63 @@
 import styled from 'styled-components';
 import { variables } from '../../style';
 
-var FaqStyleContainer = styled.div`
-  width: 80%; 
-  max-width: 600%;
-  margin: 50px auto;
-`;
-
-var FaqStyleTitle = styled.div`
-&:hover {
-  background-color: ${variables.darkWhiteSmoke};
-}
-  font-weight: bold;
-  font-size: 18px;
-  background-color: ${variables.whitesmoke};
-  width: 100%;
-  padding: 15px 20px;
-  cursor: pointer;
+const FaqStyleContainer = styled.div`
   transition: background-color 0.2s linear;
+  overflow: hidden;
+  &:hover {
+    background-color: ${variables.darkWhiteSmoke};
+    cursor: pointer;
+  }
 `;
 
-var FaqStyleContent = styled.div`: 
-  overflow: hidden;
-  color: green;
-  border-left: 1px solid ${variables.whitesmoke};
-  border-right: 1px solid ${variables.whitesmoke};
-  transition: all 0.3s ease-out;
-  height: 0;
-  opacity: 0;
-  padding: 0 0.6rem;
-  background: ${variables.whitesmoke};
-  font-weight: 200;
-  letter-spacing: 1px;
+const FaqStyleTitle = styled.div`
+  h2 {
+    font-weight: bold;
+    font-size: 18px;
+  }
+  margin: 0;
+  padding: 15px 20px;
+`;
 
-  &.open {
-    color: red; 
+const FaqStyleContent = styled.div`
+  pointer-events: none;
+  z-index: -1;
+  height: 0;
+  overflow: hidden;
+  transition: max-height 0.3s ease-out;
+  max-height: 0;
+
+  &.answer-enter {
+    opacity: 0.01;
+    max-height: 0;
+  }
+
+  &.answer-enter-active {
     opacity: 1;
+    max-height:600px;
     height: auto;
-    padding: 1rem;
+    transition: all 300ms ease-in-out;
+  }
+  &.answer-enter-done {
+    opacity: 1;
+    max-height:600px;
+    height: auto;
+  }
+
+  &.answer-exit {
+    opacity: 1;
+    max-height:600px;
+    height: auto;
+  }
+
+  &.answer-exit-active {
+    opacity: 0;
+    max-height: 0;
+    transition: all 300ms ease-in-out;
+  }
+
+  p {
+    padding: 5px 30px 25px;
   }
 `;
 
